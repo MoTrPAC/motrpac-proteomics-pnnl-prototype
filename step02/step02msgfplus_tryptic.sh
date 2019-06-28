b3f2de592e2eb208ca19d17eba6bdc859a46e026
alchemistmatt <matthew.monroe@pnnl.gov>
2019-06-28 16:52:57 -0700

    Defined MS-GF+ options using parameter files

--- step02/step02msgfplus_tryptic.sh
+++ step02/step02msgfplus_tryptic.sh
@@ -1,22 +1,16 @@
 #!/bin/sh
 
 # Execute in docker
-java -Xmx4000M -jar /app/MSGFPlus.jar \
--s /data/msgfplus_tryptic_input/MoTrPAC_Pilot_TMT_W_S1_01_12Oct17_Elm_AQ-17-09-02.mzML \
--o /data/msgfplus_tryptic_output/MoTrPAC_Pilot_TMT_W_S1_01_12Oct17_Elm_AQ-17-09-02.mzid \
--d /data/ID_007275_FB1B42E8.fasta  \
--t 20ppm -m 0 -inst 3 -e 1 -ti -1,2 -ntt 1 -tda 1 \
--minLength 6 -maxLength 50 -minCharge 2 -maxCharge 5 \
--n 1 -protocol 1 -thread 7 \
--mod /data/MSGFPlus_Mods.txt \
--minNumPeaks 5 -addFeatures 1 > /data/step02.log
+java -Xmx4000M \
+-jar /app/MSGFPlus.jar \
+-s /data/test_global/msgfplus_tryptic_input/MoTrPAC_Pilot_TMT_W_S1_01_12Oct17_Elm_AQ-17-09-02.mzML \
+-o /data/test_global/msgfplus_tryptic_output/MoTrPAC_Pilot_TMT_W_S1_01_12Oct17_Elm_AQ-17-09-02.mzid \
+-d /data/ID_007275_FB1B42E8.fasta \
+-conf /parameters/MzRef_StatCysAlk_TMT_6plex.txt > /data/test_global/step02.log
 
-java -Xmx4000M -jar /app/MSGFPlus.jar \
--s /data/msgfplus_tryptic_input/MoTrPAC_Pilot_TMT_W_S1_02_12Oct17_Elm_AQ-17-09-02.mzML \
--o /data/msgfplus_tryptic_output/MoTrPAC_Pilot_TMT_W_S1_02_12Oct17_Elm_AQ-17-09-02.mzid \
--d /data/ID_007275_FB1B42E8.fasta  \
--t 20ppm -m 0 -inst 3 -e 1 -ti -1,2 -ntt 1 -tda 1 \
--minLength 6 -maxLength 50 -minCharge 2 -maxCharge 5 \
--n 1 -protocol 1 -thread 7 \
--mod /data/MSGFPlus_Mods.txt \
--minNumPeaks 5 -addFeatures 1 >> /data/step02.log
+java -Xmx4000M \
+-jar /app/MSGFPlus.jar \
+-s /data/test_global/msgfplus_tryptic_input/MoTrPAC_Pilot_TMT_W_S1_02_12Oct17_Elm_AQ-17-09-02.mzML \
+-o /data/test_global/msgfplus_tryptic_output/MoTrPAC_Pilot_TMT_W_S1_02_12Oct17_Elm_AQ-17-09-02.mzid \
+-d /data/ID_007275_FB1B42E8.fasta \
+-conf /parameters/MzRef_StatCysAlk_TMT_6plex.txt >> /data/test_global/step02.log
--- step04/step04msgfplus.sh
+++ step04/step04msgfplus.sh
@@ -1,25 +1,16 @@
 #!/bin/sh
 
+# Execute in docker
 java -Xmx4000M \
 -jar /app/MSGFPlus.jar \
 -s /data/test_global/msgfplus_output/MoTrPAC_Pilot_TMT_W_S1_01_12Oct17_Elm_AQ-17-09-02_FIXED.mzML \
 -o /data/test_global/msgfplus_output/MoTrPAC_Pilot_TMT_W_S1_01_12Oct17_Elm_AQ-17-09-02_final.mzid \
--d /data/ID_007275_FB1B42E8.fasta  \
--t 20ppm -m 0 -inst 3 -e 1 -ti -1,2 -ntt 1 -tda 1 \
--minLength 6 -maxLength 50 -minCharge 2 -maxCharge 5 \
--n 1 -protocol 1 -thread 7 \
--mod /data/MSGFPlus_Mods.txt \
--minNumPeaks 5 \
--addFeatures 1 >> /data/test_global/step04.log
+-d /data/ID_007275_FB1B42E8.fasta \
+-conf /parameters/MSGFPlus_PartTryp_DynMetOx_Stat_CysAlk_TMT_6Plex_20ppmParTol.txt > /data/test_global/step04.log
 
 java -Xmx4000M \
 -jar /app/MSGFPlus.jar \
 -s /data/test_global/msgfplus_output/MoTrPAC_Pilot_TMT_W_S1_02_12Oct17_Elm_AQ-17-09-02_FIXED.mzML \
 -o /data/test_global/msgfplus_output/MoTrPAC_Pilot_TMT_W_S1_02_12Oct17_Elm_AQ-17-09-02_final.mzid \
--d /data/ID_007275_FB1B42E8.fasta  \
--t 20ppm -m 0 -inst 3 -e 1 -ti -1,2 -ntt 1 -tda 1 \
--minLength 6 -maxLength 50 -minCharge 2 -maxCharge 5 \
--n 1 -protocol 1 -thread 7 \
--mod /data/MSGFPlus_Mods.txt \
--minNumPeaks 5 \
--addFeatures 1 >> /data/test_global/step04.log
+-d /data/ID_007275_FB1B42E8.fasta \
+-conf /parameters/MSGFPlus_PartTryp_DynMetOx_Stat_CysAlk_TMT_6Plex_20ppmParTol.txt >> /data/test_global/step04.log
