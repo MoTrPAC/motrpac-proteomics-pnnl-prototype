52d5dba477d70cf9b272f0e37b0e8ca33cba6343
alchemistmatt <matthew.monroe@pnnl.gov>
2019-06-28 17:29:45 -0700

    Fix names of test phospho datasets

--- step03/step03a_phospho.sh
+++ step03/step03a_phospho.sh
@@ -18,7 +18,7 @@ docker run -it \
 -e WINEDEBUG=-all \
 -v $PWD/data:/data \
 chambm/pwiz-skyline-i-agree-to-the-vendor-licenses \
-wine msconvert /data/msgfplus_input/MoTrPAC_Pilot_TMT_P_S1_08_24Oct17_Elm_AQ-17-09-02.mzML \
---outfile /data/msgfplus_output/MoTrPAC_Pilot_TMT_P_S1_08_24Oct17_Elm_AQ-17-09-02_FIXED.mzML \
---filter "mzRefiner /data/msgfplus_output/MoTrPAC_Pilot_TMT_P_S1_08_24Oct17_Elm_AQ-17-09-02.mzid thresholdValue=-1e-10 thresholdStep=10 maxSteps=2" \
+wine msconvert /data/msgfplus_input/MoTrPAC_Pilot_TMT_P_S1_09_24Oct17_Elm_AQ-17-09-02.mzML \
+--outfile /data/msgfplus_output/MoTrPAC_Pilot_TMT_P_S1_09_24Oct17_Elm_AQ-17-09-02_FIXED.mzML \
+--filter "mzRefiner /data/msgfplus_output/MoTrPAC_Pilot_TMT_P_S1_09_24Oct17_Elm_AQ-17-09-02.mzid thresholdValue=-1e-10 thresholdStep=10 maxSteps=2" \
 --32 --mzML >> step03.log
--- step06/step06phrp_phospho.sh
+++ step06/step06phrp_phospho.sh
@@ -12,7 +12,7 @@ mono /app/phrp/PeptideHitResultsProcRunner.exe \
 
 
 mono /app/phrp/PeptideHitResultsProcRunner.exe \
--I:/data/test_phospho/ascore_ouput/MoTrPAC_Pilot_TMT_P_S1_08_24Oct17_Elm_AQ-17-09-02.tsv \
+-I:/data/test_phospho/ascore_ouput/MoTrPAC_Pilot_TMT_P_S1_09_24Oct17_Elm_AQ-17-09-02.tsv \
 -M:/parameters/MSGFPlus_PartTryp_DynMetOx_Stat_CysAlk_TMT_6Plex_20ppmParTol_ModDefs.txt \
 -T:/parameters/Mass_Correction_Tags.txt \
 -N:/parameters/MSGFPlus_PartTryp_DynMetOx_Stat_CysAlk_TMT_6Plex_20ppmParTol.txt \
