173d6f7d09f05b68a0dfb6facd03922e3b0fba06
alchemistmatt <matthew.monroe@pnnl.gov>
2019-06-28 17:30:12 -0700

    Update the output file path and add a phospho version of the script

--- step01/convertRaw_phospho.sh
+++ step01/convertRaw_phospho.sh
@@ -4,5 +4,5 @@ docker run -it --rm \
 -e WINEDEBUG=-all \
 -v $PWD/data:/data:rw \
 chambm/pwiz-skyline-i-agree-to-the-vendor-licenses \
-wine msconvert /data/test_global/raw/*.raw \
--o /data/test_global/msgfplus_tryptic_input/
+wine msconvert /data/test_phospho/raw/*.raw \
+-o /data/test_phospho/msgfplus_tryptic_input/
