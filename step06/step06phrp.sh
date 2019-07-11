eb55b1350077333f9f9213eebcf4688fb7020808
alchemistmatt <matthew.monroe@pnnl.gov>
2019-07-10 21:56:04 -0700

    Write the PeptideHitResultsProcessor files to directory phrp_output

--- step06/step06phrp.sh
+++ step06/step06phrp.sh
@@ -2,21 +2,23 @@
 
 mono /app/phrp/PeptideHitResultsProcRunner.exe \
 -I:/data/test_global/msgfplus_output/MoTrPAC_Pilot_TMT_W_S1_01_12Oct17_Elm_AQ-17-09-02.tsv \
+-O:/data/test_global/phrp_output/
 -M:/parameters/MSGFPlus_PartTryp_DynMetOx_Stat_CysAlk_TMT_6Plex_20ppmParTol_ModDefs.txt \
 -T:/parameters/Mass_Correction_Tags.txt \
 -N:/parameters/MSGFPlus_PartTryp_DynMetOx_Stat_CysAlk_TMT_6Plex_20ppmParTol.txt \
 -SynPvalue:0.2 -SynProb:0.05 \
--L:/data/test_global/ascore_output/PHRP_LogFile.txt \
+-L:/data/test_global/phrp_output/PHRP_LogFile.txt \
 -ProteinMods \
 -F:/data/ID_007275_FB1B42E8.revCat.fasta > /data/test_global/step06_phrp.log
 
 
 mono /app/phrp/PeptideHitResultsProcRunner.exe \
 -I:/data/test_global/msgfplus_output/MoTrPAC_Pilot_TMT_W_S1_02_12Oct17_Elm_AQ-17-09-02.tsv \
+-O:/data/test_global/phrp_output/
 -M:/parameters/MSGFPlus_PartTryp_DynMetOx_Stat_CysAlk_TMT_6Plex_20ppmParTol_ModDefs.txt \
 -T:/parameters/Mass_Correction_Tags.txt \
 -N:/parameters/MSGFPlus_PartTryp_DynMetOx_Stat_CysAlk_TMT_6Plex_20ppmParTol.txt \
 -SynPvalue:0.2 -SynProb:0.05 \
--L:/data/test_global/ascore_output/PHRP_LogFile.txt \
+-L:/data/test_global/phrp_output/PHRP_LogFile.txt \
 -ProteinMods \
 -F:/data/ID_007275_FB1B42E8.revCat.fasta >> /data/test_global/step06_phrp.log
--- step06/step06phrp_phospho.sh
+++ step06/step06phrp_phospho.sh
@@ -2,21 +2,23 @@
 
 mono /app/phrp/PeptideHitResultsProcRunner.exe \
 -I:/data/test_phospho/msgfplus_output/MoTrPAC_Pilot_TMT_P_S1_01_DIL_28Oct17_Elm_AQ-17-10-03.tsv \
+-O:/data/test_global/phrp_output/
 -M:/parameters/MSGFPlus_Tryp_DynSTYPhos_Stat_CysAlk_TMT_6Plex_Protocol1_20ppmParTol_ModDefs.txt \
 -T:/parameters/Mass_Correction_Tags.txt \
 -N:/parameters/MSGFPlus_Tryp_DynSTYPhos_Stat_CysAlk_TMT_6Plex_Protocol1_20ppmParTol.txt \
 -SynPvalue:0.2 -SynProb:0.05 \
--L:/data/test_phospho/ascore_output/PHRP_LogFile.txt \
+-L:/data/test_phospho/phrp_output/PHRP_LogFile.txt \
 -ProteinMods \
 -F:/data/ID_007275_FB1B42E8.revCat.fasta > /data/test_phospho/step06_phrp_phospho.log
 
 
 mono /app/phrp/PeptideHitResultsProcRunner.exe \
 -I:/data/test_phospho/msgfplus_output/MoTrPAC_Pilot_TMT_P_S2_01_3Nov17_Elm_AQ-17-10-03.tsv \
+-O:/data/test_global/phrp_output/
 -M:/parameters/MSGFPlus_Tryp_DynSTYPhos_Stat_CysAlk_TMT_6Plex_Protocol1_20ppmParTol_ModDefs.txt \
 -T:/parameters/Mass_Correction_Tags.txt \
 -N:/parameters/MSGFPlus_Tryp_DynSTYPhos_Stat_CysAlk_TMT_6Plex_Protocol1_20ppmParTol.txt \
 -SynPvalue:0.2 -SynProb:0.05 \
--L:/data/test_phospho/ascore_output/PHRP_LogFile.txt \
+-L:/data/test_phospho/phrp_output/PHRP_LogFile.txt \
 -ProteinMods \
 -F:/data/ID_007275_FB1B42E8.revCat.fasta >> /data/test_phospho/step06_phrp_phospho.log
