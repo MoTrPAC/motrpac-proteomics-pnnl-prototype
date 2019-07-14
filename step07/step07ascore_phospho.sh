9d905f4d877bfe1ca1b977cffcb7162722e40af9
alchemistmatt <matthew.monroe@pnnl.gov>
2019-07-14 14:28:33 -0700

    Update paths in the script for step 07

--- step07/step07ascore_phospho.sh
+++ step07/step07ascore_phospho.sh
@@ -2,18 +2,20 @@
 
 mono AScore_Console.exe \
 -T:msgfplus \
--F:MoTrPAC_Pilot_TMT_P_S1_01_DIL_28Oct17_Elm_AQ-17-10-03_syn.txt \
--D:MoTrPAC_Pilot_TMT_P_S1_01_DIL_28Oct17_Elm_AQ-17-10-03_FIXED.mzML \
--P:AScore_CID_0.5Da_ETD_0.5Da_HCD_0.05Da.xml \
+-F:/data/test_phospho/MoTrPAC_Pilot_TMT_P_S1_01_DIL_28Oct17_Elm_AQ-17-10-03_syn.txt \
+-D:/data/test_phospho/MoTrPAC_Pilot_TMT_P_S1_01_DIL_28Oct17_Elm_AQ-17-10-03_FIXED.mzML \
+-P:/parameters/AScore_CID_0.5Da_ETD_0.5Da_HCD_0.05Da.xml \
 -U:MoTrPAC_Pilot_TMT_P_S1_01_DIL_28Oct17_Elm_AQ-17-10-03_syn_plus_ascore.txt \
--L:/data/test_global/ascore_output/AScore_LogFile.txt
+-O:/data/test_phospho/
+-L:/data/test_phospho/ascore_output/AScore_LogFile.txt
 -Fasta:/data/ID_007275_FB1B42E8.fasta > /data/test_phospho/step07_ascore.log
 
 mono AScore_Console.exe \
 -T:msgfplus \
--F:MoTrPAC_Pilot_TMT_P_S2_01_3Nov17_Elm_AQ-17-10-03_syn.txt \
--D:MoTrPAC_Pilot_TMT_P_S2_01_3Nov17_Elm_AQ-17-10-03_FIXED.mzML \
--P:AScore_CID_0.5Da_ETD_0.5Da_HCD_0.05Da.xml \
+-F:/data/test_phospho/MoTrPAC_Pilot_TMT_P_S2_01_3Nov17_Elm_AQ-17-10-03_syn.txt \
+-D:/data/test_phospho/MoTrPAC_Pilot_TMT_P_S2_01_3Nov17_Elm_AQ-17-10-03_FIXED.mzML \
+-P:/parameters/AScore_CID_0.5Da_ETD_0.5Da_HCD_0.05Da.xml \
 -U:MoTrPAC_Pilot_TMT_P_S2_01_3Nov17_Elm_AQ-17-10-03_syn_plus_ascore.txt \
--L:/data/test_global/ascore_output/AScore_LogFile.txt
+-O:/data/test_phospho/
+-L:/data/test_phospho/ascore_output/AScore_LogFile.txt
 -Fasta:/data/ID_007275_FB1B42E8.fasta >> /data/test_phospho/step07_ascore.log
