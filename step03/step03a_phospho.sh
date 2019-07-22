33680334cebef9ba31144e2347f896ae3dfa32ac
David JM <biodavidjm@gmail.com>
2019-07-22 09:37:46 -0700

    Update step03 output file path

--- step03/step03a_phospho.sh
+++ step03/step03a_phospho.sh
@@ -8,9 +8,9 @@ docker run -it \
 -v $PWD/data:/data:rw \
 chambm/pwiz-skyline-i-agree-to-the-vendor-licenses \
 wine msconvert /data/test_phospho/msgfplus_input/MoTrPAC_Pilot_TMT_P_S1_01_DIL_28Oct17_Elm_AQ-17-10-03.mzML \
---outfile MoTrPAC_Pilot_TMT_P_S1_01_DIL_28Oct17_Elm_AQ-17-10-03_FIXED.mzML \
+--outfile data/test_phospho/msgfplus_output/MoTrPAC_Pilot_TMT_P_S1_01_DIL_28Oct17_Elm_AQ-17-10-03_FIXED.mzML \
 --filter "mzRefiner /data/test_phospho/msgfplus_output/MoTrPAC_Pilot_TMT_P_S1_01_DIL_28Oct17_Elm_AQ-17-10-03.mzid thresholdValue=-1e-10 thresholdStep=10 maxSteps=2" \
---32 --mzML > /data/test_phospho/step03.log
+--32 --mzML > data/test_phospho/step03.log
 
 
 docker run -it \
@@ -19,8 +19,8 @@ docker run -it \
 -v $PWD/data:/data \
 chambm/pwiz-skyline-i-agree-to-the-vendor-licenses \
 wine msconvert /data/test_phospho/msgfplus_input/MoTrPAC_Pilot_TMT_P_S2_01_3Nov17_Elm_AQ-17-10-03.mzML \
---outfile /data/test_phospho/msgfplus_output/MoTrPAC_Pilot_TMT_P_S2_01_3Nov17_Elm_AQ-17-10-03_FIXED.mzML \
+--outfile data/test_phospho/msgfplus_output/MoTrPAC_Pilot_TMT_P_S2_01_3Nov17_Elm_AQ-17-10-03_FIXED.mzML \
 --filter "mzRefiner /data/test_phospho/msgfplus_output/MoTrPAC_Pilot_TMT_P_S2_01_3Nov17_Elm_AQ-17-10-03.mzid thresholdValue=-1e-10 thresholdStep=10 maxSteps=2" \
---32 --mzML >> /data/test_phospho/step03.log
+--32 --mzML >> data/test_phospho/step03.log
 
 # mv MoTrPAC_Pilot_TMT_P_S1_01_DIL_28Oct17_Elm_AQ-17-10-03_FIXED.mzML MoTrPAC_Pilot_TMT_P_S2_01_3Nov17_Elm_AQ-17-10-03_FIXED.mzML 
