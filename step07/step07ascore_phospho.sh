12cc1d331dc87ffef5db1f4e9528676080cdbc17
David JM <biodavidjm@gmail.com>
2019-07-27 09:58:18 -0700

    MI-40 Update AScore step07

--- step07/step07ascore_phospho.sh
+++ step07/step07ascore_phospho.sh
@@ -6,7 +6,7 @@ mono /app/ascore/AScore_Console.exe \
 -D:/data/test_phospho/msgfplus_output/MoTrPAC_Pilot_TMT_P_S1_01_DIL_28Oct17_Elm_AQ-17-10-03_FIXED.mzML \
 -P:/parameters/AScore_CID_0.5Da_ETD_0.5Da_HCD_0.05Da.xml \
 -U:MoTrPAC_Pilot_TMT_P_S1_01_DIL_28Oct17_Elm_AQ-17-10-03_syn_plus_ascore.txt \
--O:/data/test_phospho/ \
+-O:/data/test_phospho/ascore_output/ \
 -L:/data/test_phospho/ascore_output/AScore_LogFile.txt \
 -Fasta:/data/ID_007275_FB1B42E8.fasta > /data/test_phospho/step07_ascore.log
 
@@ -16,6 +16,6 @@ mono /app/ascore/AScore_Console.exe \
 -D:/data/test_phospho/msgfplus_output/MoTrPAC_Pilot_TMT_P_S2_01_3Nov17_Elm_AQ-17-10-03_FIXED.mzML \
 -P:/parameters/AScore_CID_0.5Da_ETD_0.5Da_HCD_0.05Da.xml \
 -U:MoTrPAC_Pilot_TMT_P_S2_01_3Nov17_Elm_AQ-17-10-03_syn_plus_ascore.txt \
--O:/data/test_phospho/ \
--L:/data/test_phospho/ascore_output/AScore_LogFile2.txt \
+-O:/data/test_phospho/ascore_output/ \
+-L:/data/test_phospho/ascore_output/AScore_LogFile.txt \
 -Fasta:/data/ID_007275_FB1B42E8.fasta >> /data/test_phospho/step07_ascore.log
