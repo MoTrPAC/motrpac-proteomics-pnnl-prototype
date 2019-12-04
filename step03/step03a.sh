9eb6de79589053079be628ae43a31010d25d9d90
David JM <biodavidjm@gmail.com>
2019-12-04 12:15:38 -0800

    Update README
    #10
    Also includes several improvements

--- step03/step03a.sh
+++ step03/step03a.sh
@@ -1,26 +1,25 @@
 #!/bin/sh
 
-# STEP 3A
+# STEP 3a
 
-docker run -it \
---rm \
+docker run -it --rm \
 -e WINEDEBUG=-all \
 -v $PWD/data:/data:rw \
 chambm/pwiz-skyline-i-agree-to-the-vendor-licenses \
 wine msconvert /data/test_global/msgfplus_input/MoTrPAC_Pilot_TMT_W_S1_01_12Oct17_Elm_AQ-17-09-02.mzML \
---outfile /data/test_global/msgfplus_output/MoTrPAC_Pilot_TMT_W_S1_01_12Oct17_Elm_AQ-17-09-02_FIXED.mzML \
+-o /data/test_global/msgfplus_output/ \
+--outfile msgfplus_output/MoTrPAC_Pilot_TMT_W_S1_01_12Oct17_Elm_AQ-17-09-02_FIXED.mzML \
 --filter "mzRefiner /data/test_global/msgfplus_output/MoTrPAC_Pilot_TMT_W_S1_01_12Oct17_Elm_AQ-17-09-02.mzid thresholdValue=-1e-10 thresholdStep=10 maxSteps=2" \
---32 --mzML | tee data/test_global/step03.log
+--32 --mzML | tee data/test_global/step03a.log
 
 
-docker run -it \
---rm \
+docker run -it --rm \
 -e WINEDEBUG=-all \
--v $PWD/data:/data \
+-v $PWD/data:/data:rw \
 chambm/pwiz-skyline-i-agree-to-the-vendor-licenses \
 wine msconvert /data/test_global/msgfplus_input/MoTrPAC_Pilot_TMT_W_S1_02_12Oct17_Elm_AQ-17-09-02.mzML \
---outfile /data/test_global/msgfplus_output/MoTrPAC_Pilot_TMT_W_S1_02_12Oct17_Elm_AQ-17-09-02_FIXED.mzML \
+-o /data/test_global/msgfplus_output/ \
+--outfile MoTrPAC_Pilot_TMT_W_S1_02_12Oct17_Elm_AQ-17-09-02_FIXED.mzML \
 --filter "mzRefiner /data/test_global/msgfplus_output/MoTrPAC_Pilot_TMT_W_S1_02_12Oct17_Elm_AQ-17-09-02.mzid thresholdValue=-1e-10 thresholdStep=10 maxSteps=2" \
---32 --mzML | tee -a data/test_global/step03.log
+--32 --mzML | tee -a data/test_global/step03a.log
 
-# the output directory is ignored
