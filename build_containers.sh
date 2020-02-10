f783dfbe39ada0ee7beaf8c75256de7402f87893
David JM <biodavidjm@gmail.com>
2020-02-10 11:15:10 -0800

    MI-40 Update build_containers

--- build_containers.sh
+++ build_containers.sh
@@ -1,9 +1,30 @@
 #!/bin/sh
 
-# Build containers
-docker build -t "motrpac:masic" -f step00/Dockerfile .
-docker build -t "motrpac:msgfplus" -f step02/Dockerfile .
-docker build -t "motrpac:ppmerror" -f step03/Dockerfile .
-docker build -t "motrpac:mzid2tsv" -f step05/Dockerfile .
-docker build -t "motrpac:phrp" -f step06/Dockerfile .
+# Build containers Locally
+docker build -t "motrpac-prot-masic:v1.0_20200115" -f step00/Dockerfile .
+docker build -t "motrpac-prot-msgfplus:v1.0_20200115" -f step02/Dockerfile .
+docker build -t "motrpac-prot-ppmerror:v1.0_20200115" -f step03/Dockerfile .
+docker build -t "motrpac-prot-mzid2tsv:v1.0_20200115" -f step05/Dockerfile .
+docker build -t "motrpac-prot-phrp:v1.0_20200115" -f step06/Dockerfile .
 
+# Pull images not build in house
+docker pull chambm/pwiz-skyline-i-agree-to-the-vendor-licenses
+
+# Cloud Container Registry
+
+## Tag
+docker tag motrpac-prot-masic:v1.0_20200122 gcr.io/motrpac-portal-dev/motrpac-prot-masic:v1.0_20200122
+docker tag motrpac-prot-msgfplus:v1.0_20200115 gcr.io/motrpac-portal-dev/motrpac-prot-msgfplus:v1.0_20200115
+docker tag motrpac-prot-ppmerror:v1.0_20200115 gcr.io/motrpac-portal-dev/motrpac-prot-ppmerror:v1.0_20200115
+docker tag motrpac-prot-mzid2tsv:v1.0_20200115 gcr.io/motrpac-portal-dev/motrpac-prot-mzid2tsv:v1.0_20200115
+docker tag motrpac-prot-phrp:v1.0_20200115 gcr.io/motrpac-portal-dev/motrpac-prot-phrp:v1.0_20200115
+
+## push
+docker push gcr.io/motrpac-portal-dev/motrpac-prot-masic:v1.0_20200122
+docker push gcr.io/motrpac-portal-dev/motrpac-prot-msgfplus:v1.0_20200115
+docker push gcr.io/motrpac-portal-dev/motrpac-prot-ppmerror:v1.0_20200115
+docker push gcr.io/motrpac-portal-dev/motrpac-prot-mzid2tsv:v1.0_20200115
+docker push gcr.io/motrpac-portal-dev/motrpac-prot-phrp:v1.0_20200115
+
+## Pull containers from the registry
+docker pull gcr.io/motrpac-portal-dev/motrpac-prot-masic:v1.0_20200122
