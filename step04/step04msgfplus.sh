8ed40ee052d4511514e43f50a77e5cfd235ee5a3
Michael Nestor <michael.nestor@pnnl.gov>
2020-08-20 14:52:34 -0700

    drop _FIXED suffix after step 3

--- step04/step04msgfplus.sh
+++ step04/step04msgfplus.sh
@@ -3,14 +3,14 @@
 # Execute in docker
 java -Xmx4000M \
 -jar /app/MSGFPlus.jar \
--s /data/test_global/msgfplus_output/MoTrPAC_Pilot_TMT_W_S1_01_12Oct17_Elm_AQ-17-09-02_FIXED.mzML \
+-s /data/test_global/msgfplus_output/MoTrPAC_Pilot_TMT_W_S1_01_12Oct17_Elm_AQ-17-09-02.mzML \
 -o /data/test_global/msgfplus_output/MoTrPAC_Pilot_TMT_W_S1_01_12Oct17_Elm_AQ-17-09-02_final.mzid \
 -d /data/ID_007275_FB1B42E8.fasta \
 -conf /parameters/MSGFPlus_PartTryp_DynMetOx_Stat_CysAlk_TMT_6Plex_20ppmParTol.txt | tee /data/test_global/step04_msgfplus.log
 
 java -Xmx4000M \
 -jar /app/MSGFPlus.jar \
--s /data/test_global/msgfplus_output/MoTrPAC_Pilot_TMT_W_S1_02_12Oct17_Elm_AQ-17-09-02_FIXED.mzML \
+-s /data/test_global/msgfplus_output/MoTrPAC_Pilot_TMT_W_S1_02_12Oct17_Elm_AQ-17-09-02.mzML \
 -o /data/test_global/msgfplus_output/MoTrPAC_Pilot_TMT_W_S1_02_12Oct17_Elm_AQ-17-09-02_final.mzid \
 -d /data/ID_007275_FB1B42E8.fasta \
 -conf /parameters/MSGFPlus_PartTryp_DynMetOx_Stat_CysAlk_TMT_6Plex_20ppmParTol.txt | tee -a /data/test_global/step04_msgfplus.log
