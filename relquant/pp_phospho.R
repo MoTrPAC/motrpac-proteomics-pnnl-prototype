cb75c2973ec382be17f97fb0d5b9212d9f14802a
Michael Nestor <michael.nestor@pnnl.gov>
2020-08-20 11:13:30 -0700

    finished plexedpiper script for phospho data

--- relquant/pp_phospho.R
+++ relquant/pp_phospho.R
@@ -40,162 +40,59 @@ opt = list()
 opt_parser <- OptionParser(option_list = option_list)
 opt <- parse_args(opt_parser)
 
-msnid <- read_msgf_data(opt$msgf_output_folder, "_syn_plus_ascore.txt")
-
+message("- Prepare MS/MS IDs")
+message("   + Read the MS-GF+ output + Ascore")
+msnid <- read_msgf_data(opt$ascore_output_folder, "_syn_plus_ascore.txt")
 msnid <- apply_filter(msnid, "grepl(\"\\\\*\", peptide)")
 
+message("   + FDR filter")
 msnid <- filter_msgf_data_peptide_level(msnid, 0.01)
 
+message("   + Inference of parsimonius set")
 msnid <- infer_parsimonious_accessions(msnid)
 
-msnid <- remap_accessions_refseq_to_gene(msnid, organism_name="Rattus norvegicus")
+message("   + Mapping sites to protein sequence")
+fst <- Biostrings::readAAStringSet(opt$fasta_file)
+names(fst) <- sub("^([A-Z]P_\\d+\\.\\d+)\\s.*", "\\1", names(fst))
+msnid <- map_mod_sites(msnid, fst, 
+                       accession_col = "accession", 
+                       peptide_mod_col = "Peptide", 
+                       mod_char = "*",
+                       site_delimiter = "lower")
 
-path_to_FASTA_gene <- remap_accessions_refseq_to_gene_fasta(opt$fasta_file, organism_name="Rattus norvegicus")
-
-msnid <- compute_num_peptides_per_1000aa(msnid, path_to_FASTA_gene)
-
-msnid <- filter_msgf_data_protein_level(msnid, 0.01)
-
+message("   + Remove decoy sequences")
 msnid <- apply_filter(msnid, "!isDecoy")
-
-fst <- Biostrings::readAAStringSet(path_to_FASTA_gene, format="fasta", 
-                       nrec=-1L, skip=0L, use.names=TRUE)
-ids <- psms(msnid) %>%
-  distinct(accession, peptide)
-ids_with_sites <- map_mod_sites(ids, fst, "accession", "peptide", "*")
-
-ids_with_sites <- ids_with_sites %>%
-  mutate(idx = map(PepLoc, length) %>% unlist) %>%
-  filter(idx != 0) %>%
-  dplyr::select(-idx)
-
-ids_with_sites_simplified <- ids_with_sites %>%
-  dplyr::select(accession, peptide, SiteCollapsedFirst) %>%
-  mutate(site = unlist(SiteCollapsedFirst) %>%
-           gsub(",","_",.) %>%
-           paste(accession, ., sep="-")) %>%
-  dplyr::select(-SiteCollapsedFirst)
-
-psms(msnid) <- inner_join(psms(msnid), ids_with_sites_simplified)
-
-psms(msnid) <- psms(msnid) %>%
-  inner_join(ids_with_sites_simplified)
-
-
-
 
 message("- Prepare reporter ion intensities")
 message("   + Read MASIC ouput")
 path_to_MASIC_results <- opt$masic_output_folder
 masic_data <- read_masic_data(path_to_MASIC_results, interference_score=TRUE)
 
-message("- Filtering MASIC data")
+message("   + Filtering MASIC data")
 masic_data <- filter_masic_data(masic_data, 0.5, 0)
 
-#fractions <- read_tsv(system.file("extdata/study_design/fractions.txt", package = "PlexedPiperTestData"))
-#fractions <- fractions[1:2,]
-#write.table(fractions,
-#            file=paste(study_design_folder,"fractions.txt",sep="/"),
-#            quote=F, sep="\t", eol="\r\n",)
 
-message("- Read fractions.txt")
+message("- Fetch study design tables")
+message("   + Read fractions.txt")
 fractions <- read.table(paste(opt$study_design_folder,"fractions.txt",sep="/"))
 
-message("- Read samples.txt")
-#samples <- read_tsv(system.file("extdata/study_design/samples.txt", package = "PlexedPiperTestData"))
-#samples <- samples[1:10,]
-#write.table(samples,
-#            file=paste(study_design_folder,"samples.txt",sep="/"),
-#            quote=F, sep="\t", eol="\r\n",)
-samples <- read.table(paste(study_design_folder,"samples.txt",sep="/"))
+message("   + Read samples.txt")
+samples <- read.table(paste(opt$study_design_folder,"samples.txt",sep="/"))
 
-message("- Read reference.txt")
-#references <- filter(samples, ReporterAlias == "ref")
-#names(references)[names(references) == "ReporterAlias"] <- "Reference"
-#references <- references[c('PlexID', 'QuantBlock', 'Reference')]
-#write.table(references,
-#            file=paste(study_design_folder,"references.txt",sep="/"),
-#            quote=F, sep="\t", eol="\r\n",)
-references <- read.table(paste(study_design_folder,"references.txt",sep="/"))
+message("   + Read references.txt")
+references <- read.table(paste(opt$study_design_folder,"references.txt",sep="/"))
 
-message("- Creating quantitative cross-tab")
-aggregation_level <- c("accession")
-quant_cross_tab <- create_crosstab(msnid, 
-                                   masic_data, 
-                                   aggregation_level, 
-                                   fractions, samples, references)
-
-write.table(quant_cross_tab,
-            file=paste(opt$plexedpiper_output_folder,"quant_cross_tab.txt",sep="/"),
-            quote=F, sep="\t", eol="\r\n",)
-
-unlink(".Rcache", recursive=TRUE)
-
-
-
-
-
-
-# remap to site notation
-# needed: 1) accession, 
-#         2) fasta with accession, 
-#         3) peptide sequence with asterisc location
-# Let's borrow vp.misc remapping. !!! Ultimately it should be moved to MSnID
-fst <- readAAStringSet(path_to_FASTA_gene, format="fasta", 
-                       nrec=-1L, skip=0L, use.names=TRUE)
-ids <- psms(msnid) %>%
-  distinct(accession, peptide)
-ids_with_sites <- map_PTM_sites(ids, fst, "accession", "peptide", "*")
-
-ids_with_sites <- ids_with_sites %>%
-  mutate(idx = map(PepLoc, length) %>% unlist) %>%
-  filter(idx != 0) %>%
-  dplyr::select(-idx)
-
-ids_with_sites_simplified <- ids_with_sites %>%
-  dplyr::select(accession, peptide, SiteCollapsedFirst) %>%
-  mutate(site = unlist(SiteCollapsedFirst) %>%
-           gsub(",","_",.) %>%
-           paste(accession, ., sep="-")) %>%
-  dplyr::select(-SiteCollapsedFirst)
-
-psms(msnid) <- inner_join(psms(msnid), ids_with_sites_simplified)
-
-psms(msnid) <- psms(msnid) %>%
-  inner_join(ids_with_sites_simplified)
-
-
-
-
-
-
-masic_data <- read_masic_data_from_DMS(3432, interference_score = T)
-masic_data <- filter_masic_data(masic_data, 0.5, 0)
-
-library(dplyr)
-fractions <- masic_data %>%
-  distinct(Dataset) %>%
-  mutate(PlexID = sub("SHSY5Y_IIS_3x3_P_(B\\d).*","\\1",Dataset))
-head(fractions)
-
-library(readr)
-samples <- read_tsv("../../3432/samples.txt")
-head(samples,10)
-
-ref <- "(`10min_control1`*`10min_control2`*`10min_Insulin`*`10min_IGF1`*`10min_IGF2`*`60min_control1`*`60min_control2`*`60min_Insulin`*`60min_IGF1`*`60min_IGF2`)^(1/10)"
-references <- samples %>%
-  distinct(PlexID, QuantBlock) %>%
-  mutate(Reference = ref)
-
-
-aggregation_level <- c("site")
+message("- Create cross-tab")
+aggregation_level <- c("SiteID")
 quant_cross_tab <- create_crosstab(msnid, 
                                    masic_data, 
                                    aggregation_level, 
                                    fractions, samples, references)
 dim(quant_cross_tab)
 head(quant_cross_tab)
-```
 
+write.table(quant_cross_tab,
+            file=paste(opt$plexedpiper_output_folder,"quant_cross_tab.txt",sep="/"),
+            quote=F, sep="\t", eol="\r\n",)
 
-
+unlink(".Rcache", recursive=TRUE)
