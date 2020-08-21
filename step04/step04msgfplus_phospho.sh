fb0fd58e8ae18cc563c0cb193f48d60f2167970b
Michael Nestor <michael.nestor@pnnl.gov>
2020-08-21 10:14:14 -0700

    remove _FIXED suffix from .mzML file after step03

--- step04/step04msgfplus_phospho.sh
+++ step04/step04msgfplus_phospho.sh
@@ -3,14 +3,14 @@
 # Execute in docker
 java -Xmx4000M \
 -jar /app/MSGFPlus.jar \
--s /data/test_phospho/msgfplus_output/MoTrPAC_Pilot_TMT_P_S1_01_DIL_28Oct17_Elm_AQ-17-10-03_FIXED.mzML \
+-s /data/test_phospho/msgfplus_output/MoTrPAC_Pilot_TMT_P_S1_01_DIL_28Oct17_Elm_AQ-17-10-03.mzML \
 -o /data/test_phospho/msgfplus_output/MoTrPAC_Pilot_TMT_P_S1_01_DIL_28Oct17_Elm_AQ-17-10-03_final.mzid \
 -d /data/ID_007275_FB1B42E8.fasta \
 -conf /parameters/MSGFPlus_Tryp_DynSTYPhos_Stat_CysAlk_TMT_6Plex_Protocol1_20ppmParTol.txt | tee /data/test_phospho/step04_msgfplus.log
 
 java -Xmx4000M \
 -jar /app/MSGFPlus.jar \
--s /data/test_phospho/msgfplus_output/MoTrPAC_Pilot_TMT_P_S2_01_3Nov17_Elm_AQ-17-10-03_FIXED.mzML \
+-s /data/test_phospho/msgfplus_output/MoTrPAC_Pilot_TMT_P_S2_01_3Nov17_Elm_AQ-17-10-03.mzML \
 -o /data/test_phospho/msgfplus_output/MoTrPAC_Pilot_TMT_P_S2_01_3Nov17_Elm_AQ-17-10-03_final.mzid \
 -d /data/ID_007275_FB1B42E8.fasta \
 -conf /parameters/MSGFPlus_Tryp_DynSTYPhos_Stat_CysAlk_TMT_6Plex_Protocol1_20ppmParTol.txt | tee -a /data/test_phospho/step04_msgfplus.log
--- step07/step07ascore_phospho.sh
+++ step07/step07ascore_phospho.sh
@@ -3,7 +3,7 @@
 mono /app/ascore/AScore_Console.exe \
 -T:msgfplus \
 -F:/data/test_phospho/phrp_output/MoTrPAC_Pilot_TMT_P_S1_01_DIL_28Oct17_Elm_AQ-17-10-03_syn.txt \
--D:/data/test_phospho/msgfplus_output/MoTrPAC_Pilot_TMT_P_S1_01_DIL_28Oct17_Elm_AQ-17-10-03_FIXED.mzML \
+-D:/data/test_phospho/msgfplus_output/MoTrPAC_Pilot_TMT_P_S1_01_DIL_28Oct17_Elm_AQ-17-10-03.mzML \
 -P:/parameters/AScore_CID_0.5Da_ETD_0.5Da_HCD_0.05Da.xml \
 -U:MoTrPAC_Pilot_TMT_P_S1_01_DIL_28Oct17_Elm_AQ-17-10-03_syn_plus_ascore.txt \
 -O:/data/test_phospho/ascore_output/ \
@@ -13,7 +13,7 @@ mono /app/ascore/AScore_Console.exe \
 mono /app/ascore/AScore_Console.exe \
 -T:msgfplus \
 -F:/data/test_phospho/phrp_output/MoTrPAC_Pilot_TMT_P_S2_01_3Nov17_Elm_AQ-17-10-03_syn.txt \
--D:/data/test_phospho/msgfplus_output/MoTrPAC_Pilot_TMT_P_S2_01_3Nov17_Elm_AQ-17-10-03_FIXED.mzML \
+-D:/data/test_phospho/msgfplus_output/MoTrPAC_Pilot_TMT_P_S2_01_3Nov17_Elm_AQ-17-10-03.mzML \
 -P:/parameters/AScore_CID_0.5Da_ETD_0.5Da_HCD_0.05Da.xml \
 -U:MoTrPAC_Pilot_TMT_P_S2_01_3Nov17_Elm_AQ-17-10-03_syn_plus_ascore.txt \
 -O:/data/test_phospho/ascore_output/ \
