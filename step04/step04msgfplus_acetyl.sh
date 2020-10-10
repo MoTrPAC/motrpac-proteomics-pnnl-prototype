3aa826d986c4831c951d216e7238a687958e03e9
alchemistmatt <matthew.monroe@pnnl.gov>
2020-10-09 17:33:00 -0700

    Update the MS-GF+ parameter file used for acetyl datasets

--- step04/step04msgfplus_acetyl.sh
+++ step04/step04msgfplus_acetyl.sh
@@ -6,4 +6,4 @@ java -Xmx4000M \
 -s /data/test_acetyl/msgfplus_output/P_MoTrPAC_1A_RM_Plex001_G_f06_01May19_Arwen_REP-19-04-r02.mzML \
 -o /data/test_acetyl/msgfplus_output/P_MoTrPAC_1A_RM_Plex001_G_f06_01May19_Arwen_REP-19-04-r02_final.mzid \
 -d /data/ID_007275_FB1B42E8.fasta \
--conf /parameters/MSGFPlus_PartTryp_DynMetOx_TMTExclusive_K_Acetyl_Stat_CysAlk_TMT_6Plex_20ppmParTol.txt | tee /data/test_acetyl/step04_msgfplus.log
+-conf /parameters/MSGFPlus_Tryp_DynMetOx_TMTExclusive_K_Acetyl_K_Carbamyl_N_Deamid_Stat_CysAlk_TMT_6Plex_20ppmParTol_PlusOne_IsotopeError.txt
