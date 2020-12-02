a1fac63fb7c009807af7d05fa02e3087cb7e384d
David JM <biodavidjm@gmail.com>
2020-12-02 09:08:17 -0800

    Update ppmcharter

--- step03/step03b_ubiq.sh
+++ step03/step03b_ubiq.sh
@@ -6,4 +6,4 @@ mono /app/PPMErrorCharterPython.exe \
 -I:/data/test_ubiq/msgfplus_output/MS2_KggTMT_LenMM1S_MethodComparison_1mg_01.mzid \
 -F:/data/test_ubiq/mzrefiner_output/MS2_KggTMT_LenMM1S_MethodComparison_1mg_01_FIXED.mzML \
 -EValue:1E-10 \
--O:/data/test_global/ppm_errorcharter/ | tee /data/test_ubiq/step03b.log
+-O:/data/test_ubiq/ppm_errorcharter/ | tee /data/test_ubiq/step03b.log
