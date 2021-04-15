28f843661720cfa46dee841120b7da62519d795e
Michael Nestor <michael.nestor@pnnl.gov>
2021-04-15 15:18:18 -0700

    updated PTM relquant scripts

--- relquant/relquant_acetyl.sh
+++ relquant/relquant_acetyl.sh
@@ -3,7 +3,7 @@
 Rscript /relquant/pp_acetyl.R \
 -i /data/test_acetyl/phrp_output \
 -j /data/test_acetyl/masic_output \
--g /data/test_global/plexedpiper_output \
+-g /data/test_global/plexedpiper_output/results_ratio.txt \
 -f /data/ID_007275_FB1B42E8.fasta \
 -s /relquant/study_design_acetyl \
 -o /data/test_acetyl/plexedpiper_output
