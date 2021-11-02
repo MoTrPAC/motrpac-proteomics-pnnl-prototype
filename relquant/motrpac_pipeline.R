4b4ef243ece9759cdf387de21e2e15e127463998
Michael Nestor <47347033+mdnestor@users.noreply.github.com>
2021-11-02 12:35:52 -0700

    Unified global and PTM pipelines

---
+++ relquant/motrpac_pipeline.R
@@ -0,0 +1,181 @@
+
+#' @param msnid (MSnID)
+#' @param path_to_FASTA (character)
+#' @param masic_data (data.frame)
+#' @param ascore (data.frame)
+#' @param proteomics (character)
+#' @param study_design (list)
+#' @param species (character)
+#' @param annotation (character)
+#' @param global_results (character) Default is `NULL`.
+#' @param output_folder (character)
+#' @param file_prefix (character)
+#' @param save_env (logical)
+#' @param verbose (logical) Default is `FALSE`.
+#' @return (list)
+#' @importFrom Biostrings readAAStringSet
+#' @importFrom PlexedPiper read_study_design filter_masic_data
+#' make_rii_peptide_gl make_results_ratio_gl make_rii_peptide_ph
+#' make_results_ratio_ph
+#' 
+#' @examples
+#' out <- motrpac_pnnl_pipeline(msnid, path_to_FASTA, masic_data, ascore=NULL,
+#'                              proteomics = "pr",
+#'                              study_design,
+#'                              species = "Rattus norvegicus",
+#'                              annotation = "RefSeq")
+#' save_pnnl_pipeline_results(out, output_folder = "./global/output/")
+#' 
+#' out <- motrpac_pnnl_pipeline(msnid, path_to_FASTA, masic_data, ascore,
+#'                              proteomics = "ph",
+#'                              study_design,
+#'                              species = "Rattus norvegicus",
+#'                              annotation = "RefSeq")
+#' save_pnnl_pipeline_results(out, output_folder = "./phospho/output/")
+
+#' @export
+motrpac_pnnl_pipeline <- function(msnid, path_to_FASTA, masic_data,
+                                  ascore = NULL, proteomics,
+                                  study_design, species, annotation,
+                                  global_results = NULL, output_folder = ".",
+                                  file_prefix = NULL, save_env = FALSE, verbose = TRUE) {
+  if (verbose) {
+    message("Running MoTrPAC PNNL pipeline with the following parameters:")
+    message("proteomics: \"pr\".")
+    message("Study design folder: ", study_design_folder, "\".")
+    message("Species: \"", species, "\".")
+    message("Annotation: \"", annotation, "\".")
+  }
+  
+  fst <- Biostrings::readAAStringSet(path_to_FASTA)
+  names(fst) <- sub("^(\\S*)\\s.*", "\\1", names(fst))
+  
+  if (verbose) message("Filtering MS-GF+ results.")
+  if (proteomics == "pr") {
+    if (verbose) message("   + Correct for isotope selection error")
+    msnid <- correct_peak_selection(msnid)
+  }
+  
+  if (proteomics != "pr") {
+    if (verbose) message("   + Select best PTM location by AScore")
+    msnid <- best_PTM_location_by_ascore(msnid, ascore)
+    
+    if (verbose) message("   + Apply PTM filter")
+    if (proteomics == "ph") {
+      reg.expr <- "grepl(\"\\\\*\", peptide)"
+    } else if (proteomics == "ac") {
+      reg.expr <- "grepl(\"#\", peptide)"
+    } else if (proteomics == "ub") {
+      psms(msnid) <- psms(msnid) %>% mutate(peptide = gsub("@", "#", peptide))
+      reg.expr <- "grepl(\"#\", peptide)"
+    }
+    msnid <- apply_filter(msnid, reg.expr)
+  }
+  
+  message("   + Peptide-level FDR filter")
+  msnid <- filter_msgf_data(msnid, level = "peptide", fdr.max = 0.01)
+  
+  if (proteomics == "pr") {
+    if (verbose) message("   + Protein-level FDR filter")
+    msnid <- compute_num_peptides_per_1000aa(msnid, path_to_FASTA = fasta_file)
+    
+    msnid <- filter_msgf_data(msnid, level = "accession", fdr.max = 0.01)
+  }
+  
+  message("   + Remove decoy sequences")
+  msnid <- apply_filter(msnid, "!isDecoy")
+  
+  if (verbose) message("   + Concatenating redundant RefSeq matches")
+  msnid <- assess_redundant_protein_matches(msnid, collapse = ",")
+  
+  if (verbose) message("   + Assessing non-inferable proteins")
+  msnid <- assess_noninferable_proteins(msnid, collapse = ",")
+  
+  message("   + Inference of parsimonius set")
+  if (proteomics == "pr") {
+    prior <- character(0)
+  } else if (is.null(global_results)) {
+    message("     > Reference global proteomics dataset NOT provided")
+    prior <- character(0)
+  } else {
+    global_ratios <- read.table(global_results, header=T, sep="\t")
+    prior <- unique(global_ratios$protein_id)
+  }
+  msnid <- infer_parsimonious_accessions(msnid, prior = prior)
+  
+  fst <- readAAStringSet(path_to_FASTA)
+  names(fst) <- sub("^(\\S*)\\s.*", "\\1", names(fst))
+  
+  if (proteomics == "pr") {
+    if (verbose) message("   + Compute protein coverage")
+    msnid <- compute_accession_coverage(msnid, fst)
+  }
+  
+  if (proteomics != "pr") {
+    message("   + Mapping sites to protein sequence")
+    if (proteomics == "ph") {
+      mod_char = "*"
+    } else if (proteomics %in% c("ac", "ub")) {
+      mod_char = "#"
+    } else {
+      stop("Proteomics variable not supported.")
+    }
+    
+    msnid <- map_mod_sites(msnid,
+                           fasta           = fst,
+                           accession_col   = "accession",
+                           peptide_mod_col = "peptide", 
+                           mod_char        = mod_char,
+                           site_delimiter  = "lower")
+    
+    message("   + Map flanking sequences")
+    msnid <- extract_sequence_window(msnid, fasta = fst)
+  }
+  
+  if (verbose) message("Filtering MASIC results.")
+  masic_data <- filter_masic_data(masic_data, 0.5, 0)
+  
+  args <- list(msnid      = msnid, 
+               masic_data = masic_data, 
+               fractions  = study_design$fractions, 
+               samples    = study_design$samples, 
+               references = study_design$references, 
+               annotation = annotation,
+               org_name   = species)
+  
+  if (verbose) message("Making results tables.")
+  if (proteomics == "pr") {
+    rii_peptide   <- do.call(make_rii_peptide_gl,   args)
+    results_ratio <- do.call(make_results_ratio_gl, args)
+  } else {
+    rii_peptide   <- do.call(make_rii_peptide_ph,   args)
+    results_ratio <- do.call(make_results_ratio_ph, args)
+  }
+  
+  if (verbose) message("Saving results.")
+  
+  if (!is.null(output_folder)) {
+    if (!dir.exists(file.path(output_folder))) {
+      dir.create(file.path(output_folder), recursive = TRUE)
+    }
+    
+    filename = paste0(file_prefix, "results_RII-peptide.txt")
+    write.table(rii_peptide,
+                file      = file.path(output_folder, filename),
+                sep       = "\t",
+                row.names = FALSE,
+                quote     = FALSE)
+    
+    filename = paste0(file_prefix, "results_ratio.txt")
+    write.table(results_ratio,
+                file      = file.path(output_folder, filename),
+                sep       = "\t",
+                row.names = FALSE,
+                quote     = FALSE)
+    if (save_env) {
+      save.image(file = file.path(output_folder, "env.RData"))
+    }
+  }
+  if (verbose) message("Done!")
+  return(list(rii_peptide = rii_peptide, results_ratio = results_ratio))
+}
--- relquant/pp.R
+++ relquant/pp.R
@@ -11,6 +11,8 @@ suppressPackageStartupMessages(suppressWarnings(library(dplyr)))
 suppressPackageStartupMessages(suppressWarnings(library(PlexedPiper)))
 
 option_list <- list(
+  make_option(c("-p", "--proteomics"), type="character", default=NULL, 
+              help="Proteomics experiment: pr/ph/ub/ac", metavar="character"),
   make_option(c("-i", "--msgf_output_folder"), type="character", default=NULL, 
               help="MSGF output folder", metavar="character"),
   make_option(c("-j", "--masic_output_folder"), type="character", default=NULL, 
@@ -19,13 +21,19 @@ option_list <- list(
               help="FASTA file (RefSeq format)", metavar="character"),
   make_option(c("-s", "--study_design_folder"), type="character", default=NULL, 
               help="Study design folder", metavar="character"),
-  make_option(c("-o", "--plexedpiper_output_folder"), type="character", default=NULL, 
-              help="PlexedPiper output folder (Crosstabs)", metavar="character"),
-  make_option(c("-n", "--plexedpiper_output_name_prefix"), type="character", default=NULL,
-              help="PlexedPiper output folder (Crosstabs)", metavar="character"),
   make_option(c("-c", "--species"), type="character", default=NULL,
               help="Full scientific name for the species (e.g. -c \"Homo sapiens\")", metavar="character")
+  make_option(c("-d", "--annotation"), type="character", default=NULL,
+              help="Name of Protein database (either RefSeq or UniProt)", metavar="character")
+  make_option(c("-a", "--ascore_output_folder"), type="character", default=NULL, 
+              help="AScore output folder", metavar="character"),
+  make_option(c("-g", "--plexedpiper_global_results_ratio"), type="character", default=NULL, 
+              help="PlexedPiper global results ratio table", metavar="character"),
+  make_option(c("-o", "--plexedpiper_output_folder"), type="character", default=NULL, 
+              help="PlexedPiper output folder (Crosstabs)", metavar="character")
 )
+
+
 
 get_date <- function(){
   # GET QC_DATE----
@@ -39,13 +47,15 @@ get_date <- function(){
 opt_parser <- OptionParser(option_list = option_list)
 opt <- parse_args(opt_parser)
 
-if (is.null(opt$msgf_output_folder) | 
+if (is.null(opt$proteomics) | 
+    is.null(opt$msgf_output_folder) | 
     is.null(opt$masic_output_folder) | 
     is.null(opt$fasta_file) |
     is.null(opt$study_design_folder) |
-    is.null(opt$plexedpiper_output_folder) | 
-    is.null(opt$species)
-    ){
+    is.null(opt$species) |
+    is.null(opt$annotation) |
+    is.null(opt$plexedpiper_output_folder)
+    ) {
   print_help(opt_parser)
   stop("Required arguments are missed", call.=FALSE)
 }
@@ -60,12 +70,7 @@ if (is.null(opt$msgf_output_folder) |
 # plexedpiper_output_folder = "data/test_global/plexedpiper_output300"
 # To DEBUG ---------------------------------------------------------------------
 
-msgf_output_folder <- opt$msgf_output_folder 
-masic_output_folder <- opt$masic_output_folder 
-fasta_file <- opt$fasta_file
-study_design_folder <- opt$study_design_folder
-plexedpiper_output_folder <- opt$plexedpiper_output_folder
-species <- opt$species
+
 
 date2print <- get_date()
 if(is.null(opt$plexedpiper_output_name_prefix)){
@@ -75,117 +80,32 @@ if(is.null(opt$plexedpiper_output_name_prefix)){
   plexedpiper_output_name_prefix <- paste0(plexedpiper_output_name_prefix, "_", date2print)
 }
 
+# Data loading
 message("- Fetch study design tables")
-study_design <- read_study_design(study_design_folder)
+study_design <- read_study_design(opt$study_design_folder)
 
-fractions  <- study_design$fractions
-samples    <- study_design$samples
-references <- study_design$references
+msnid <- read_msgf_data(opt$msgf_output_folder)
 
+ascore <- read_AScore_results(opt$ascore_output_folder)
 
-message("- Prepare MS/MS IDs")
-message("   + Read the MS-GF+ output")
-msnid <- read_msgf_data(path_to_MSGF_results = msgf_output_folder)
-
-if (!setequal(fractions$Dataset, msnid$Dataset)) {
-  stop("Datasets in MS-GF+ output and 'fractions.txt' do not match!")
-}
-
-message("   + Correct for isotope selection error")
-msnid <- correct_peak_selection(msnid)
-
-message("   + MS/MS ID filter and peptide level")
-msnid <- filter_msgf_data_peptide_level(msnid, 0.01)
-
-message("   + MS/MS ID filter at protein level")
-msnid <- compute_num_peptides_per_1000aa(msnid, 
-                                         path_to_FASTA = fasta_file)
-
-msnid <- filter_msgf_data_protein_level(msnid, 0.01)
-
-message("   + Remove decoy accessions")
-msnid <- apply_filter(msnid, "!isDecoy")
-
-message("   + Concatenating redundant RefSeq matches")
-msnid <- assess_redundant_protein_matches(msnid)
-
-message("   + Assessing non-inferable proteins")
-msnid <- assess_noninferable_proteins(msnid)
-
-message("   + Inference of parsimonious protein set")
-msnid <- infer_parsimonious_accessions(msnid)
-
-message("   + Compute protein coverage")
-suppressMessages(
-  fst <- Biostrings::readAAStringSet(fasta_file)
-)
-names(fst) <- sub("^(\\S*)\\s.*", "\\1", names(fst))
-msnid <- compute_accession_coverage(msnid, fst)
-
-message("- Prepare reporter ion intensities")
-message("   + Read MASIC ouput")
-masic_data <- read_masic_data(path_to_MASIC_results = masic_output_folder, 
+masic_data <- read_masic_data(opt$masic_output_folder, 
                               interference_score = TRUE)
 
-if (!setequal(fractions$Dataset, masic_data$Dataset)) {
-  stop("Datasets in MASIC output and 'fractions.txt' do not match!")
-}
-
-message("   + Filtering MASIC data")
-masic_data <- filter_masic_data(masic_data, 0.5, 0)
-
-
-message("   + Generate RII peptide. ", appendLF = FALSE)
-rii_peptide <- suppressMessages(
-  suppressPackageStartupMessages(
-    suppressWarnings(
-      make_rii_peptide_gl(msnid = msnid, 
-                          masic_data = masic_data, 
-                          fractions = fractions, 
-                          samples = samples, 
-                          references = references, 
-                          org_name = species)
-    )
-  )
-)
-message("Total number of unique protein_id: ", length(unique(rii_peptide$protein_id)))
-
-message("   + Make results ratio.", appendLF = FALSE)
-results_ratio <- suppressMessages(
-  suppressPackageStartupMessages(
-    suppressWarnings(
-      make_results_ratio_gl(msnid =  msnid, 
-                            masic_data = masic_data, 
-                            fractions = fractions, 
-                            samples = samples, 
-                            references = references, 
-                            org_name = species)
-    )
-  )
-)
-message("Total number of unique protein_id: ", length(unique(results_ratio$protein_id)))
-
-message("- Save results")
-
-if(!dir.exists(file.path(plexedpiper_output_folder))){
-  dir.create(file.path(plexedpiper_output_folder), recursive = TRUE)
-}
-
-file_rii <- paste0(plexedpiper_output_name_prefix, "_results_RII-peptide.txt")
-file_ratio <- paste0(plexedpiper_output_name_prefix, "_results_ratio.txt")
-write.table(rii_peptide,
-            file = paste(plexedpiper_output_folder, file_rii, sep="/"),
-            sep="\t",
-            row.names = FALSE,
-            quote = FALSE)
-
-write.table(results_ratio,
-            file = paste(plexedpiper_output_folder, file_ratio, sep="/"),
-            sep="\t",
-            row.names = FALSE,
-            quote = FALSE)
-
-message("- Done!")
+# Pipeline call
+source("motrpac_pipeline.R")
+out <- motrpac_pnnl_pipeline(msnid          = msnid,
+                             path_to_FASTA  = opt$fasta_file,
+                             masic_data     = masic_data,
+                             ascore         = ascore,
+                             proteomics     = tolower(opt$proteomics),
+                             study_design   = study_design,
+                             species        = opt$species,
+                             annotation     = opt$annotation,
+                             global_results = opt$plexedpiper_global_results_ratio,
+                             output_folder  = opt$plexedpiper_output_folder,
+                             file_prefix    = plexedpiper_output_name_prefix,
+                             save_env       = FALSE,
+                             verbose        = TRUE)
 
 unlink(".Rcache", recursive=TRUE)
 
